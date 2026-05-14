e9a775fed75b9a1e7c4c9b2e91cb3354a8d1f70f
lulultfh <punyalulu05@gmail.com>
2026-05-15 03:26:53 +0700

    feat: add api configuration, app colors for styling dan format currency

---
+++ lib/core/constant/api_config.dart
@@ -0,0 +1,4 @@
+class ApiConfig {
+  static const String baseUrl = "http://10.0.2.2:3000/api/";
+  static const String imgBaseUrl = "http://10.0.2.2:3000/uploads/";
+}
---
+++ lib/core/constant/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // AppColors._();
+  static const Color primaryColor = Color(0xff213448);
+  static const Color backgroundColor = Color(0xffECEFCA);
+  static const Color blue1Color = Color(0xff94B4C1);
+  static const Color blue2Color = Color(0xff547792);
+}
