58e64becfc88bbd39b3f6aa13f2fb44965212143
lulultfh <punyalulu05@gmail.com>
2026-05-15 03:31:29 +0700

    feat: memanggil bloc auth dan setup halaman login as default routenya

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/data/repositories/auth_repository.dart';
+import 'package:frontend/logic/bloc/auth/auth_bloc.dart';
+import 'package:frontend/logic/bloc/auth/auth_event.dart';
+import 'package:frontend/logic/debug/bloc_observer.dart';
+import 'package:frontend/ui/pages/auth/login_page.dart';
 
 void main() {
+  Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
 
@@ -10,27 +17,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return BlocProvider(
+      create: (_) => AuthBloc(repository: AuthRepository())..add(AppStarted()),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          
+          colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const LoginPage()
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
